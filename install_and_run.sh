f1d6de30070ad64e8cb7fa2e5720b34a4dc1cb6b
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-21 04:08:34 +0800

    Create install_and_run.sh

---
+++ install_and_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 使用 curl 下载并直接运行安装脚本
+curl -s https://raw.githubusercontent.com/small-haozi/xrayr-onecheck/main/install.sh | bash
+
+# 执行 haha 脚本并传递参数
+haha "$MY_PARAM1" "$MY_PARAM2" "$MY_PARAM3" "$MY_PARAM4" "$MY_PARAM5" "$MY_PARAM6" "$MY_PARAM7"
