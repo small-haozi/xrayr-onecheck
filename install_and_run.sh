a1c579d02e1ea8172dc8ba9041eef852a0fc5ce1
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-21 04:08:49 +0800

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -4,4 +4,4 @@
 curl -s https://raw.githubusercontent.com/small-haozi/xrayr-onecheck/main/install.sh | bash
 
 # 执行 haha 脚本并传递参数
-haha "$MY_PARAM1" "$MY_PARAM2" "$MY_PARAM3" "$MY_PARAM4" "$MY_PARAM5" "$MY_PARAM6" "$MY_PARAM7"
+haha "$1" "$2" "$3" "$4" "$5" "$6" "$7"
